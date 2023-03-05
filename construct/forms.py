1c85bb58efd27d2d59f9f244acad9c21cc99906a
Uran87 <kapilskij@mail.ru>
2023-03-05 18:54:45 +0300

    2 commit

--- construct/forms.py
+++ construct/forms.py
@@ -6,7 +6,7 @@ class ContactForm(ModelForm):
     class Meta:
         model = Requests
         fields = ['name_customer', 'phone_number', 'text_request']
-        widgets = {"name_customer": TextInput(attrs={"class": "modal-input", "placeholder": "Введите Ваше имя", 'size': '40',"maxlength":"10"}),
+        widgets = {"name_customer": TextInput(attrs={"class": "modal-input", "placeholder": "Введите Ваше имя","maxlength":"10"}),
                    "phone_number": TextInput(attrs={"class": "modal-input", "placeholder": "Введите номер телефона"}),
                    "text_request": Textarea(attrs={"class": "modal-input-area", "placeholder": "Ваш комментарий"})}
 
